755de377cf243c8ea7cd580fbff99d233c13d7b9
John <jakajohn@web.de>
2023-01-31 10:46:14 +0100

    modified:  ANCHER+Prettier footer-login.css 	modified:  ANCHER+Prettier header-login.css 	modified:  Prettier login.js 	modified: ANCHER+Prettier  main-login.css 	modified: ANCHER+Prettier  settings.css

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-
 //this function toggles the password to visibility//
 function showPassword() {
   let x = document.getElementById("password");
@@ -9,30 +8,24 @@ function showPassword() {
   }
 }
 
-
 //this function build the colors for the login-button//
 
 // Get the button element by its ID
 let button = document.getElementById("myButton");
 
 // Add an event listener for the mouseover event
-button.addEventListener("mouseover", function() {
+button.addEventListener("mouseover", function () {
   // Change the background color of the button to black
   button.style.backgroundColor = "black";
   // Change the text color of the button to white
   button.style.color = "white";
   myButton.style.borderColor = "white";
-  
 });
 
-button.addEventListener("mouseout", function() {
+button.addEventListener("mouseout", function () {
   button.classList.remove("white-border");
   button.style.backgroundColor = "white";
   // Change the text color of the button back to black
   button.style.color = "black";
   myButton.style.borderColor = "black";
 });
-
-
-
-
